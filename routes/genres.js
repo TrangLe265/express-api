5f7d9112157cae7e55dc7ad8ab948387bafba71e
TrangLe265 <trang.le@myy.haaga-helia.fi>
2026-02-18 12:48:59 +0200

    added the middleware the validateObjectId

--- routes/genres.js
+++ routes/genres.js
@@ -1,22 +1,33 @@
 const express = require('express');
 const router = express.Router();
-const {Genre, validate} = require('../models/genre')
+const {Genre, validate} = require('../models/genre'); 
+const validateObjId = require('../middleware/validateObjId'); 
 
 router.get('/', async (req,res) => {
-  const genres = await Genre.find()
-    .select("-__v") // select("-__v") = do not include version of the object when sending info back
-    .sort("name");
+    const genres = await Genre.find()
+        .select("-__v") // select("-__v") = do not include version of the object when sending info back
+        .sort("name");
 
     res.send(genres); 
   
-})
+}); 
 
-router.get('/:id', async(req,res) => {
-    const genre = await Genre.findById(req.params.id).select("-__v"); 
-    if (!genre) return res.status(404).send(`No resource found with the provided id`); 
-    res.send(genre);
-})
+router.put('/:id',validateObjId ,async(req,res) => {
+    const {error} = validate(req.body); 
+    if (error) return res.status(400).send(error.details[0].message); 
 
+    const genre = await Genre.findByIdAndUpdate(
+        req.params.id, 
+        {name : req.body.name } , 
+        {
+            new: true //db returns old obj by default, here we tell db to return the updated one instead
+        }
+    );
+
+    if (!genre) return res.status(404).send(`No resource found with the provided id`);
+
+    res.send(genre); 
+}); 
 
 router.post('/', async(req,res) => {
     const {error} = validate(req.body); 
@@ -26,6 +37,22 @@ router.post('/', async(req,res) => {
     genre = await genre.save(); 
 
     res.send(genre); 
+}); 
+
+router.delete('/:id', validateObjId, async(req,res) => {
+    const result = await Genre.findByIdAndDelete(req.params.id); 
+
+    if (!result) return res.status(404).send(`No resource found with the provided id`); 
+
+    res.status(200).send(`Successfully delete`); 
+}); 
+
+router.get('/:id',validateObjId,async(req,res) => {
+    const genre = await Genre.findById(req.params.id).select("-__v"); 
+
+    if (!genre) return res.status(404).send(`No resource found with the provided id`); 
+
+    res.send(genre);
 }); 
 
 module.exports = router; 
