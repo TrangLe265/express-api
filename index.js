056cf55b244da5adbde979b51c1ee0bf11e027cb
TrangLe265 <trang.le@myy.haaga-helia.fi>
2026-02-19 12:07:30 +0200

    added customer routes

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const Joi = require('joi'); //Joi is a class, so we should name it with Pascal convention
 const dotenv = require('dotenv');
 const mongoose = require('mongoose'); //API to work with Mongo
-const genres = require('./routes/genres')
+const genres = require('./routes/genres');
+const customers = require('./routes/customers');
 const express = require('express');
+
 const app = express();
 
 app.use(express.json()); //a middleware that enables express to handle json
 app.use('/api/genres', genres); 
+app.use('/api/customers', customers); 
 
 
 mongoose.connect('mongodb://localhost/video')
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Couldnt connect to MongoDB')); 
+
 dotenv.config({path: '.env'}); 
 const port = process.env.PORT || 3000; 
-console.log(port);
 app.listen(port, () => console.log(`Server is running on port ${port} `)); 
